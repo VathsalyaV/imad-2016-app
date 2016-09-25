c511743174837369d7509572fdb438bd1d58d464
VathsalyaV <vathsalya3victorious@gmail.com>
2016-09-25 17:46:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+
+var counter=0;
+var button=document.getElementById('counter');
+button.onclick(){
+    counter=counter+1;
+    var count=document.getElementById('count');
+    count.innerHtml=counter.toString();
+}
