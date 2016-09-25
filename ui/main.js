adec3f106823ee7084903512c03b17538d09034b
VathsalyaV <vathsalya3victorious@gmail.com>
2016-09-25 18:02:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,15 @@
 
 var counter=0;
-var button = document.getElementById("counter");
-button.onclick = function() {
+var button = document.getElementById('counter');
+
+button.onClick = function() {
+    
+    //var request = new XMLHttpRequest();
+   // request.onReadyStateChange = function() {
+        
+    //}
+    
     counter=counter+1;
-    var count=document.getElementById("count");
+    var count=document.getElementById('count');
     count.innerHtml=counter.toString();
 };
