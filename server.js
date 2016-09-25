31d2c550d7e370ee4979ef74256fd800a5309cac
VathsalyaV <vathsalya3victorious@gmail.com>
2016-09-25 17:47:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter' , funtion(req , res)){
+app.get('/counter' , function(req , res) {
     counter=counter+1;
     res.send(counter.toString());
 });
