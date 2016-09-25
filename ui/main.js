998da3940f7b1b19e96d47d818be851d7c75d253
VathsalyaV <vathsalya3victorious@gmail.com>
2016-09-25 18:29:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,21 @@
 
-var counter=0;
 var button = document.getElementById('counter');
 
 button.onclick = function() {
     
-    //var request = new XMLHttpRequest();
-   // request.onReadyStateChange = function() {
-        
-    //}
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            if(request.status == 200)
+            {
+                var counter = request.responseText;
+                var count=document.getElementById('count');
+                count.innerHtml=counter.toString();
+            }
+        }
+    };
     
-    counter=counter+1;
-    var count=document.getElementById('count');
-    count.innerHtml=counter.toString();
+    request.open('GET', 'http://vathsalyav.imad.hasura-app.io', true);
+    request.send(null);
 };
